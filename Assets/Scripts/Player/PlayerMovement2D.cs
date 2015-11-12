63ae54f634bde0b936c06019d3b8739ac49a8f82
Minorueatslegs <klim@tundramobile.com>
2015-11-12 20:47:19 +0200

    Very basic movement behaviour

---
+++ Assets/Scripts/Player/PlayerMovement2D.cs
@@ -0,0 +1,76 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PlayerMovement2D : MonoBehaviour
+{
+    private const float kVelocitySmoothingFactor = 1f;
+    private static Vector3 position;
+    private bool isAccelerating;
+    private Rigidbody body;
+
+    public static Vector3 PlayerPosition
+    {
+        get
+        {
+            return position;
+        }
+    }
+
+    private void Start()
+    {
+        this.body = this.GetComponent<Rigidbody>();
+        this.StartCoroutine("BringPlayerBack");
+        this.StartCoroutine("BringPlayerDown");
+    }
+
+ 
+    void Update()
+    {
+        position = this.transform.position;
+        if (this.isAccelerating)
+        {
+            this.body.AddForce(new Vector3(0.1f, 2f), ForceMode.VelocityChange);
+        }
+        this.body.velocity = new Vector3(this.body.velocity.x, Mathf.Clamp(this.body.velocity.y, -11f, 8f));
+    }
+
+    IEnumerator BringPlayerBack()
+    {
+        for (;;)
+        {
+            if (transform.position.x < 9f)
+            {
+
+                this.body.velocity = new Vector3(this.body.velocity.x + 0.2f, this.transform.position.y);  
+            }
+            else if (transform.position.x > 17f)
+            {
+                this.body.velocity = Vector3.left;
+            }
+            yield return null;
+        }
+    }
+
+    IEnumerator BringPlayerDown()
+    {
+        for (;;)
+        {
+            if (transform.position.y > 9f)
+            {
+                this.body.velocity = new Vector3(this.body.velocity.x, - kVelocitySmoothingFactor);
+            }
+            yield return null;
+        }
+    }
+
+    public void BeginAcceleration()
+    {
+        this.isAccelerating = true;
+    }
+
+    public void EndAcceleration()
+    {
+        this.isAccelerating = false;
+    }
+
+}
