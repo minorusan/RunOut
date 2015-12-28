3012026ab06dc3fd1d7124594979f468cd3e88f1
Klim Schukin <minorusan8@gmail.com>
2015-12-28 10:31:01 +0200

    Beginnning music implementation

---
+++ Assets/Scripts/Utility/MusicModule.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using UnityEngine;
+
+namespace AssemblyCSharp
+{
+	public class MusicModule
+	{
+		private string _curPath  ;
+		private List<String> _curDirectoryFolderPaths = new List<String>() ;
+
+		public void GetCurDirFolders ()
+		{
+			_curPath = Directory.GetCurrentDirectory ();
+			foreach (var folderPath in Directory.GetDirectories( _curPath )) {
+				try {
+					_curDirectoryFolderPaths.Add (folderPath);
+
+				} catch (Exception ex) {
+					Debug.Log (ex.Message);
+				}
+			}
+			Debug.Log ("Found " + _curDirectoryFolderPaths.Count.ToString () + " Folder(s) in this Directory ");
+		}
+	}
+}
+
